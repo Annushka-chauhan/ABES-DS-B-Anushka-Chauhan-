b75892404c7e2f69d3a794ab68149d53b6b5ac1d
anushka chauhan <anushkachauhanannu@gmail.com>
2025-10-14 11:06:44 +0530

    New Learning

---
+++ JavaScript/ES6/Dom.js
@@ -0,0 +1,8 @@
+
+//This is actual dom 
+const div = document.createElement("div")
+const h1 =document.createElement("h1")
+h1.innerText = "Python is create AI"
+div.appendChild(h1)
+const id = document.getElementById("root")
+id.appendChild(div)
